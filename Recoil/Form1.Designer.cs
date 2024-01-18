cbeb2476fb816856d67a44a23ea804d24a2f4f8f
Noah-Tantner <noahtant383@ed.amdsb.ca>
2024-01-18 10:22:25 -0500

    New Base. Be careful guys

--- Recoil/Form1.Designer.cs
+++ Recoil/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.testLabel = new System.Windows.Forms.Label();
             this.minigunTimer = new System.Windows.Forms.Timer(this.components);
+            this.shotgunTimer = new System.Windows.Forms.Timer(this.components);
+            this.testLabel2 = new System.Windows.Forms.Label();
+            this.countingTimer = new System.Windows.Forms.Timer(this.components);
+            this.testLabel3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -53,12 +57,43 @@
             // 
             this.minigunTimer.Tick += new System.EventHandler(this.minigunTimer_Tick);
             // 
+            // shotgunTimer
+            // 
+            this.shotgunTimer.Interval = 2000;
+            this.shotgunTimer.Tick += new System.EventHandler(this.shotgunTimer_Tick);
+            // 
+            // testLabel2
+            // 
+            this.testLabel2.AutoSize = true;
+            this.testLabel2.Location = new System.Drawing.Point(166, 9);
+            this.testLabel2.Name = "testLabel2";
+            this.testLabel2.Size = new System.Drawing.Size(73, 16);
+            this.testLabel2.TabIndex = 1;
+            this.testLabel2.Text = "Version 1.0";
+            // 
+            // countingTimer
+            // 
+            this.countingTimer.Enabled = true;
+            this.countingTimer.Interval = 1;
+            this.countingTimer.Tick += new System.EventHandler(this.countingTimer_Tick);
+            // 
+            // testLabel3
+            // 
+            this.testLabel3.AutoSize = true;
+            this.testLabel3.Location = new System.Drawing.Point(311, 9);
+            this.testLabel3.Name = "testLabel3";
+            this.testLabel3.Size = new System.Drawing.Size(73, 16);
+            this.testLabel3.TabIndex = 2;
+            this.testLabel3.Text = "Version 1.0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SeaGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.testLabel3);
+            this.Controls.Add(this.testLabel2);
             this.Controls.Add(this.testLabel);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -71,6 +106,8 @@
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +118,10 @@
         private System.Windows.Forms.Timer gameTimer;
         private System.Windows.Forms.Label testLabel;
         private System.Windows.Forms.Timer minigunTimer;
+        private System.Windows.Forms.Timer shotgunTimer;
+        private System.Windows.Forms.Label testLabel2;
+        private System.Windows.Forms.Timer countingTimer;
+        private System.Windows.Forms.Label testLabel3;
     }
 }
 
--- Recoil/Form1.cs
+++ Recoil/Form1.cs
@@ -12,14 +12,16 @@ namespace Recoil
 {
     public partial class Form1 : Form
     {
-        bool mouseIsClicked = false;
-
         //noah's globals
         float playerYSpeed = 0f;
         float playerXSpeed = 0f;
         float maxPlayerSpeed = 10f;
 
+        int shotgunAmmo = 2;
+        int shotgunAmmoMax = 2;
         int bulletSpeed = 25;
+        int enemyHealth = 5;
+        int count = 0;
 
         double spread;
         double xStep;
@@ -29,15 +31,45 @@ namespace Recoil
         double angle;
         //
 
+        //dylan's globals
+        Rectangle aim = new Rectangle(0, 0, 5, 5);
+        bool shotgunfire = false;
+        bool minigunfire = false;
+        bool pistolfire = false;
+        bool rocketfire = false;
+        Rectangle explode = new Rectangle(0, 0, 10, 10);
+        Random randomSpread = new Random();
+        Point endpoint;
+        //
+
+        //alistair's gllobals
+        //Map information
+        int wallWidth = 25;
+
+        int barrelDimentions = 25;
+        int enemyDimentions = 20;
+        int spikeWidth = 10;
+        int spikeHeight = 20;
+
+        List<Rectangle> walls = new List<Rectangle>();
+        List<Rectangle> enemies = new List<Rectangle>();
+        List<Rectangle> strongEnemies = new List<Rectangle>();
+        List<Rectangle> spikes = new List<Rectangle>();
+        List<Rectangle> explosiveBarrels = new List<Rectangle>();
+        List<Rectangle> landMines = new List<Rectangle>();
+        //
+
+
+
         bool wDown = false;
         bool sDown = false;
         bool aDown = false;
         bool dDown = false;
 
-        SolidBrush beigeBrush = new SolidBrush(Color.Beige);
-        SolidBrush redBrush = new SolidBrush(Color.Red);
-        SolidBrush goldBrush = new SolidBrush(Color.Gold);
-        SolidBrush hotpink = new SolidBrush(Color.HotPink);
+        SolidBrush wallBrush = new SolidBrush(Color.Beige);
+        SolidBrush enemyBrush = new SolidBrush(Color.Red);
+        SolidBrush playerBrush = new SolidBrush(Color.Gold);
+        SolidBrush bulletBrush  = new SolidBrush(Color.HotPink);
 
         Pen stock = new Pen(Color.BurlyWood, 6);
 
@@ -45,7 +77,7 @@ namespace Recoil
         List<double> bulletSpeedsY = new List<double>();
         List<double> bulletSpeedsX = new List<double>();
 
-        Rectangle testEnemy = new Rectangle(200, 400, 20, 20);
+        Rectangle testEnemy = new Rectangle(200, 400, 40, 40);
         Rectangle player = new Rectangle(500, 800, 20, 20);
 
 
@@ -72,28 +104,16 @@ namespace Recoil
             gameTimer.Enabled = true;
 
             gameState = "running";
-           
+
         }
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
-
-
-            //any functions beginning with base will be in the base for all of us, they're meant for
-            //testing I advice against changing or removing them to begin with
-
-            //also, change the last parameter of this function to change the player speed
-
-            
-
-            
-
-            //BaseMovePlayer(aDown, dDown, wDown, sDown, ref player, 8);
-            BaseMoveBullets();
-            
+            moveBullets();
             //remember guys, just call your own functions here
-            
-            advancedMovement(wDown, sDown, aDown, dDown, ref player, ref playerYSpeed, ref playerXSpeed, 1f, 1.20f, 0.90f);
+            noahFunction();
+            dylanFunction();
+
             player.X += (int)playerXSpeed;
             player.Y += (int)playerYSpeed;
 
@@ -119,6 +139,32 @@ namespace Recoil
                 case Keys.Escape:
                     this.Close();
                     break;
+                    //Dylan keys
+                case Keys.D1:
+                    pistolfire = true;
+                    shotgunfire = false;
+                    minigunfire = false;
+                    rocketfire = false;
+                    break;
+                case Keys.D2:
+                    pistolfire = false;
+                    shotgunfire = true;
+                    minigunfire = false;
+                    rocketfire = false;
+                    break;
+                case Keys.D3:
+                    pistolfire = false;
+                    shotgunfire = false;
+                    minigunfire = true;
+                    rocketfire = false;
+                    break;
+                case Keys.D4:
+                    pistolfire = false;
+                    shotgunfire = false;
+                    minigunfire = false;
+                    rocketfire = true;
+                    break;
+
             }
         }
 
@@ -140,11 +186,11 @@ namespace Recoil
                     break;
             }
         }
-
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
-            BaseShootPistol();
-            mouseIsClicked = true;
+            shootPistol();
+            ShootShotgun();
+            RocketLauncher();
         }
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
@@ -159,19 +205,43 @@ namespace Recoil
 
             else if (gameState == "running")
             {
-                e.Graphics.FillEllipse(redBrush, testEnemy);
-                e.Graphics.FillEllipse(beigeBrush, player);
-
+                if (enemyHealth > 0)
+                {
+                    e.Graphics.FillEllipse(enemyBrush, testEnemy);
+                }
+                e.Graphics.FillEllipse(wallBrush, player);
+                
 
                 for (int i = 0; i < bullets.Count; i++)
                 {
-                    e.Graphics.FillEllipse(hotpink, bullets[i]);
+                    e.Graphics.FillEllipse(bulletBrush , bullets[i]);
                 }
+
+
+                //dylan's stuff
+                endpoint.X = player.X + 10 + (int)(20 * Math.Cos(angle * Math.PI / 180.0));
+                endpoint.Y = player.Y + 10 + (int)(20 * Math.Sin(angle * Math.PI / 180.0));
+
+                e.Graphics.DrawLine(stock, player.X + 10, player.Y + 10, endpoint.X, endpoint.Y);
+                //
+
+                //noah's stuff\
+
+                //
+
+                //alistair's stuff
+                for (int i = 0; i < walls.Count; i++)
+                {
+                    e.Graphics.FillRectangle(wallBrush, walls[i]);
+                }
+
+                //
+
 
             }
         }
-        
-        //basefunctions here
+
+        //basefunctions here. Let's just not change these alright guys?
         public void BaseMovePlayer(bool leftButton, bool rightButton, bool upButton, bool downButton, ref Rectangle player, int playerSpeed)
         {
 
@@ -179,7 +249,7 @@ namespace Recoil
             {
                 player.Y -= playerSpeed;
             }
-            if (downButton == true )
+            if (downButton == true)
             {
                 player.Y += playerSpeed;
             }
@@ -192,41 +262,78 @@ namespace Recoil
                 player.X += playerSpeed;
             }
         }
-        public void BaseMoveBullets()
+        public void moveBullets()
         {
             for (int i = 0; i < bullets.Count; i++)
             {
                 int x = (int)Math.Round(bullets[i].X + bulletSpeedsX[i], 0);
                 int y = (int)Math.Round(bullets[i].Y + bulletSpeedsY[i], 0);
                 bullets[i] = new Rectangle(x, y, 5, 5);
+                
+                //Put this next bit on bullet move
+                if (bullets[i].IntersectsWith(explode))
+                {
+                    bullets[i] = new Rectangle(explode.X, explode.Y, 30, 30);
+                    bullets.RemoveAt(i);
+                    bulletSpeedsX.RemoveAt(i);
+                    bulletSpeedsY.RemoveAt(i);
+                }
+                if (bullets[i].IntersectsWith(testEnemy))
+                {
+                    bullets.RemoveAt(i);
+                    bulletSpeedsX.RemoveAt(i);
+                    bulletSpeedsY.RemoveAt(i);
+                    enemyHealth--;
+                    count = 0;
+                }
+
 
             }
-        }
-        public void BaseShootPistol()
-        {
-            funMath();
 
-            Rectangle newBullet = new Rectangle(player.X + 10, player.Y + 10, 5, 5);
-            bullets.Add(newBullet);
-            bulletSpeedsX.Add(xStep * bulletSpeed);
-            bulletSpeedsY.Add(yStep * bulletSpeed);
-            applyRecoil(25);
         }
+        public void shootPistol()
+        {
+            if (pistolfire)
+            {
+                funMath();
+                Rectangle newBullet = new Rectangle(player.X + 10, player.Y + 10, 5, 5);
+                bullets.Add(newBullet);
+                bulletSpeedsX.Add(xStep * bulletSpeed);
+                bulletSpeedsY.Add(yStep * bulletSpeed);
+                applyRecoil(22);
+            }
+        }
+        //
 
         //individual functions begin here
         public void noahFunction()
         {
+            advancedMovement(wDown, sDown, aDown, dDown, ref player, ref playerYSpeed, ref playerXSpeed, 1f, 1.20f, 0.90f);
+            limitPlayArea();
+            testLabel.Text = $"Shotgun Ammo: {shotgunAmmo}";
+            testLabel2.Text = $"Enemy Health: {enemyHealth}";
+            testLabel3.Text = $"Count = {count}";
+            if(shotgunAmmo < shotgunAmmoMax)
+            {
+                shotgunTimer.Enabled = true;
+            }
+            else shotgunTimer.Enabled = false;  
+            if (count < 10)
+            {
+                enemyBrush = new SolidBrush(Color.Pink);
+            }
+            else enemyBrush = new SolidBrush(Color.Red);
         }
         public void dylanFunction()
         {
-
+            funMath();
         }
-
         public void alistairFunction()
         {
-
+            CheckCollisions();  //gameTimer.Tick
+            LoadMapSegment1();  //Game init
         }
-
+        //and end here
 
 
 
@@ -235,12 +342,8 @@ namespace Recoil
         //confusion
 
         //Noah's area
-
-        //call this method if you want accelerated, physics based movement. Beware though,
-        //there's a LOT of parameters
-        public void advancedMovement(bool upButton, bool downButton, bool leftButton, bool rightButton, ref Rectangle player, ref float YplayerSpeed, ref float XplayerSpeed, float startingSpeed, float playerAcceleration, float playerDecceleration) 
+        public void advancedMovement(bool upButton, bool downButton, bool leftButton, bool rightButton, ref Rectangle player, ref float YplayerSpeed, ref float XplayerSpeed, float startingSpeed, float playerAcceleration, float playerDecceleration)
         {
-            testLabel.Text = downButton.ToString();
             if (playerYSpeed < maxPlayerSpeed && playerYSpeed > -maxPlayerSpeed)
             {
                 if (upButton == true && player.Y > 0)
@@ -293,7 +396,7 @@ namespace Recoil
                 }
             }
 
-            if ((!upButton && !downButton) || (YplayerSpeed > maxPlayerSpeed || YplayerSpeed < - maxPlayerSpeed))
+            if ((!upButton && !downButton) || (YplayerSpeed > maxPlayerSpeed || YplayerSpeed < -maxPlayerSpeed))
             {
                 if (YplayerSpeed > 0 || YplayerSpeed < 0)
                 {
@@ -325,16 +428,13 @@ namespace Recoil
         {
             funMath();
             //angle = angle * -1;
-           playerXSpeed -= (float) xStep * recoilStrength;
-            playerYSpeed -= (float) yStep * recoilStrength;
+            playerXSpeed -= (float)xStep * recoilStrength;
+            playerYSpeed -= (float)yStep * recoilStrength;
         }
-
-
-
         public void funMath()
         {
-            Rectangle aim = new Rectangle(0, 0, 5, 5);
-            
+
+
 
             aim.X = MousePosition.X;
             aim.Y = MousePosition.Y;
@@ -345,19 +445,112 @@ namespace Recoil
             xStep = Math.Cos(angle * Math.PI / 180);
             yStep = Math.Sin(angle * Math.PI / 180);
         }
-
-
+        public void limitPlayArea()
+        {
+            if(player.X > this.Width - player.Width)
+            {
+                player.X = this.Width - player.Width;
+            }
+            if(player.Y > this.Height - player.Height)
+            {
+                player.Y = this.Height - player.Height;
+            }
+            if(player.X < 0)
+            {
+                player.X = 0;
+            }
+            if(player.Y < 0)
+            {
+                player.Y = 0;
+            }
+        }
+        private void shotgunTimer_Tick(object sender, EventArgs e)
+        {
+            shotgunAmmo++;
+        }
+        private void countingTimer_Tick(object sender, EventArgs e)
+        {
+            count++;
+        }
+        //
 
         //Dylan's area
         private void minigunTimer_Tick(object sender, EventArgs e)
         {
-
+            if (minigunfire == true)
+            {
+                Rectangle newBullet = new Rectangle(player.X + 10, player.Y + 10, 5, 5);
+                bullets.Add(newBullet);
+                bulletSpeedsX.Add(xStep * bulletSpeed);
+                bulletSpeedsY.Add(yStep * bulletSpeed);
+            }
         }
+        private void ShootShotgun()
+        {
+            if (shotgunfire == true && shotgunAmmo != 0)
+            {
+                funMath();
+                for (int i = 0; i < 5; i++)
+                {
+                    spread = randomSpread.Next(-20, 21);
+                    angle = Math.Atan2(deltaY, deltaX) * 180 / Math.PI + spread;
+
+                    xStep = Math.Cos(angle * Math.PI / 180);
+                    yStep = Math.Sin(angle * Math.PI / 180);
+                    Rectangle newBullet = new Rectangle(player.X + 10, player.Y + 10, 5, 5);
+                    bullets.Add(newBullet);
+                    bulletSpeedsX.Add(xStep * bulletSpeed);
+                    bulletSpeedsY.Add(yStep * bulletSpeed);
+                }
+                applyRecoil(32);
+                shotgunAmmo--;
+            }
+        }
+        public void RocketLauncher()
+        {
+            if (rocketfire == true)
+            {
+                Rectangle newBullet = new Rectangle(player.X + 10, player.Y + 10, 15, 15);
+                bullets.Add(newBullet);
+                bulletSpeedsX.Add(xStep * bulletSpeed);
+                bulletSpeedsY.Add(yStep * bulletSpeed);
+                explode.X = aim.X;
+                explode.Y = aim.Y;
+            }
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            minigunTimer.Enabled = true;
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            minigunTimer.Enabled = false;
+        }
+        //
 
         //Alistair's area
+        public void LoadMapSegment1()
+        {
+            Rectangle rightWall = new Rectangle(0, 0, wallWidth, this.Height);
+            Rectangle leftWall = new Rectangle(this.Width - wallWidth, 0, wallWidth, this.Height);
 
+            walls.Add(rightWall);
+            walls.Add(leftWall);
+        }
+        public void CheckCollisions()
+        {
+            for (int i = 0; i < walls.Count; i++)
+            {
+                //if (walls[i].IntersectsWith(player) && playerSpeed < 0)
+                //{
 
+                //}
+            }
+        }
+        //
 
 
     }
 }
+
+
